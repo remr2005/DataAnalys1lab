367740bec0eb828cbc5a9e7b16e8369aa0de5a3f
Kemran <morpeh2005ka@gmail.com>
2024-09-11 00:41:32 +0300

    start fix first task

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import gcd
 
 def main():
-    print(gcd.binary_alg(8,20123))
+    print(gcd.sub(3,2))
+    print("GCD for 8 and some number")
+    print(gcd.gcd_eight(20124))
+    print("Write a code that removes a vowel entered twice in the input string.")
+
 
 if  __name__ == "__main__":
     main()
