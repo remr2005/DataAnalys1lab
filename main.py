0b78a5aa01c39a3845fdab38b49fd75a162ac771
Kemran <morpeh2005ka@gmail.com>
2024-09-11 16:53:48 +0300

    testing

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import gcd
+from gcd import gcd_eight
 from twice_remove import remove_twice
 from prime_number_gen import gen
 
 def main():
     print("GCD for 8 and some number")
-    print(gcd.gcd_eight(int(input("Input number for gcd: "))))
+    print(gcd_eight(int(input("Input number for gcd: "))))
     print("Write a code that removes a vowel entered twice in the input string: ")
     print(remove_twice(input("Input string")))
     print("prime number generator")
