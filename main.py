02c717a7942a446c83491cf4702a8eac0aaba8f1
Kemran <morpeh2005ka@gmail.com>
2024-09-11 19:30:21 +0300

    done 10 task

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from prime_number_gen import gen
 from matrix import only_complex
 from frac import Frac
 import fibonachi
+from descr import Descr
 
 def main():
-    # print("GCD for 8 and some number")
-    # print(gcd_eight(int(input("Input number for gcd: "))))
-    # print("Write a code that removes a vowel entered twice in the input string: ")
-    # print(remove_twice(input("Input string")))
-    # print("prime number generator")
-    # print(gen(int(input("Input your number: "))))
-    # print(only_complex([1, 2, 3.0, True, 1+2j], [0+0j,1+6j,3,2,False]))
-    # print([i for i in fibonachi.FibSums(int(input("Input N for Fibonachi sums: ")))])
-    # print(fibonachi.find_in_fibonachi(int(input("enter your number"))))
+    print("GCD for 8 and some number")
+    print(gcd_eight(int(input("Input number for gcd: "))))
+    print("Write a code that removes a vowel entered twice in the input string: ")
+    print(remove_twice(input("Input string")))
+    print("prime number generator")
+    print(gen(int(input("Input your number: "))))
+    print(only_complex([1, 2, 3.0, True, 1+2j], [0+0j,1+6j,3,2,False]))
+    print([i for i in fibonachi.FibSums(int(input("Input N for Fibonachi sums: ")))])
+    print(fibonachi.find_in_fibonachi(int(input("enter your number"))))
     a = Frac(-1,-3)
     print("Sum 1/3 of -1/2")
     a.sum(Frac(1,-2))
@@ -24,6 +25,14 @@ def main():
     a.print_me()
     print("Mne len' perevodit' poetomu skaju chto eto obrashenie")
     print(a.obrash())
+    b = Descr(((0,0),(0,2),(2,2),(2,0)))
+    c = Descr(((0,0),(0,2),(2,2),(2,0)))
+    print(f"b = {b.coord}")
+    print(f"c = {c.coord}")
+    print("Is the descr equal? (b and c)", b.is_equal(c))
+    d = Descr(((0,0),(0,1),(1,1),(1,0)))
+    print(f"c = {d.coord}")
+    print("Is the descr similar? (b and d)", b.is_similar(d))
 
 if  __name__ == "__main__":
     main()
